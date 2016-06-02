e9a914e1cfe87ce256664f903cdb88e61be9c981
doufukobe <fpdsast@163.com>
2016-06-02 21:00:24 +0800

    改为继承自FragmentActivity

--- app/src/main/java/com/fpd/slamdunk/CommenActivity.java
+++ app/src/main/java/com/fpd/slamdunk/CommenActivity.java
@@ -1,9 +1,11 @@
 package com.fpd.slamdunk;
 
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 
 /**
  * Created by t450s on 2016/6/1.
  */
-public class CommenActivity extends Activity {
+public class CommenActivity extends FragmentActivity {
+
 }
