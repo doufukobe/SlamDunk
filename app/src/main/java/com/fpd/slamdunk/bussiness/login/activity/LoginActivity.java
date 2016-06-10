74aa51efb09795296d5915ddad3d6702bdd07a44
solo-map <470331416@qq.com>
2016-06-10 21:04:30 +0800

    myfragment2次提交

--- app/src/main/java/com/fpd/slamdunk/bussiness/login/activity/LoginActivity.java
+++ app/src/main/java/com/fpd/slamdunk/bussiness/login/activity/LoginActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -98,6 +99,7 @@ public class LoginActivity extends CommenActivity implements View.OnClickListene
     }
     private void showToast(String str)
     {
+        Log.i("TAG","showToast");
         Toast.makeText(this, str, Toast.LENGTH_SHORT).show();
     }
 
